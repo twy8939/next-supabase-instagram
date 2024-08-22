8fc8648777da396c3277064a0323e3a40a9147bc
twy8939 <twy8939@naver.com>
2024-08-22 23:30:11 +0900

    feat : Add Supabase getSession

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import LogoutButton from "components/logout-button";
+import { createServerSupabaseClient } from "utils/supabase/server";
 
 export const metadata = {
   title: "Instagram",
   description: "Instagram Clone",
 };
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createServerSupabaseClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
   return (
     <main className="w-full h-screen flex flex-col gap-2 items-center justify-center">
-      <h1 className="text-xl font-bold">Welcome {`"Kim YongMin"`}</h1>
+      <h1 className="text-xl font-bold">
+        Welcome {`${session?.user?.email?.split("@")?.[0]}`}
+      </h1>
       <LogoutButton />
     </main>
   );
